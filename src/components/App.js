b3af41cf28a1d9d009e9fc4863755dd999902e64
Pooja Birajdar <poojabirajdar23@gmail.com>
2022-03-28 16:10:03 +0530

    init

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,10 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+{/* <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> */}
 
+import React from 'react'
+
+ function App() {
+  return (
+    <div>App</div>
+  )
+}
+export default App;
